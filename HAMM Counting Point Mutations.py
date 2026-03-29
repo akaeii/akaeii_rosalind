38e69183d0a99db09bc1225875b116b6f043e280
Akaeii <magpoc.joshua@protonmail.com>
2026-03-30 03:25:56 +0800

    Corrected dataset path.

--- HAMM Counting Point Mutations.py
+++ HAMM Counting Point Mutations.py
@@ -11,7 +11,7 @@ def dH(s1,s2):
     return dist
 
 if __name__ == "__main__":
-    with open("../rosalind_hamm.txt") as file:
+    with open("./datasets/rosalind_hamm.txt", "r") as file:
         s1, s2 = file.readlines()        
 
     print(dH(s1, s2))
